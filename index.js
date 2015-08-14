53c62880b6d40b4a12dfad8f71cc571c9352657b
Tom MacWright <tom@macwright.org>
2015-08-14 12:41:58 -0400

    Make the ES5 version the main version.
    This removes babelify and simplifies how you would use this module.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var React = require('react');
 
 /**
@@ -6,12 +10,13 @@ var React = require('react');
  * focused on an input element calls an onEvent property.
  */
 var Submittable = React.createClass({
+  displayName: 'Submittable',
   propTypes: {
     children: React.PropTypes.any.isRequired,
     onEnter: React.PropTypes.func.isRequired,
     onCancel: React.PropTypes.func
   },
-  onKeyDown: function(event) {
+  onKeyDown: function onKeyDown(event) {
     if (event.target.tagName === 'INPUT') {
       if (event.keyCode === 13) {
         this.props.onEnter(event);
@@ -20,11 +25,11 @@ var Submittable = React.createClass({
       }
     }
   },
-  render: function() {
-    return (
-      <div onKeyDown={this.onKeyDown} {...this.props}>
-        {this.props.children}
-      </div>
+  render: function render() {
+    return React.createElement(
+      'div',
+      _extends({ onKeyDown: this.onKeyDown }, this.props),
+      this.props.children
     );
   }
 });
